3103dc209c057a08ff64ae4fb7888e9e2f16c6fd
Akashgowda-hub <gowdaakash1703@gmail.com>
2024-11-30 22:43:01 +0530

    Update2 test.py

--- test.py
+++ test.py
@@ -1,15 +1,1 @@
-# Prompting the user for input
-age_input = input("Enter your age: age_input ")
-
-# Converting the input to an integer
-age = int(age_input)
-
-# Checking conditions based on user input
-if age < 0:
-    print("Please enter a valid age.")
-elif age < 18:
-    print("You are a minor.")
-elif age >= 18 and age < 65:
-    print("You are an adult.")
-else:
-    print("You are a senior citizen.")
+print('Hello, world!')
